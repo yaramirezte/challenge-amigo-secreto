5b7e5001ed72b522522b1a4aafdf49c1f67329db
Yeni Andrea Ramirez Tellez <yaramirezte@gmail.com>
2025-07-25 23:18:58 +0100

    crear funciones en boton añadir

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombresDeAmigos=[];
+
+
+function agregarAmigo(){
+    if(document.getElementById("amigo").value == ""){
+        alert("Por favor, inserte un nombre.");
+
+    }else{
+        nombresDeAmigos.push(document.getElementById("amigo").value);
+        cleanCaja()
+        console.log(nombresDeAmigos);       
+        
+    }
+    agregarLista(nombresDeAmigos);
+    nombresDeAmigos.length=0;  
+    //document.getElementById("listaAmigos").innerHTML = "";
+    
+       
+}
+
+
+
+function agregarLista(elementos){
+    const ul=document.getElementById("listaAmigos");
+    let li=0
+    for(i=0;i<elementos.length;i++){
+        li=document.createElement("li");
+        li.textContent=elementos[i];
+        ul.appendChild(li); 
+        console.log(li);        
+    }
+    
+
+}
+
+function cleanCaja(){
+    document.querySelector("#amigo").value="";
+}
