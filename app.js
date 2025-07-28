4d1e6484ec66ba3f81d99747be578b07d93bf542
Yeni Andrea Ramirez Tellez <yaramirezte@gmail.com>
2025-07-28 10:51:20 +0100

    aplicar mejores practicas y corregir el codigo en app.js

--- app.js
+++ app.js
@@ -3,60 +3,47 @@ let nombresDeAmigos=[];
 
 
 function agregarAmigo(){
-    if(document.getElementById("amigo").value == ""){
+    const input=document.getElementById("amigo");
+    const nombre=input.value.trim();
+
+    if(nombre == ""){
         alert("Por favor, inserte un nombre.");
-
     }else{
-        nombresDeAmigos.push(document.getElementById("amigo").value);
-
-        cleanCaja()
-
-        console.log(nombresDeAmigos); 
-
-        agregarLista(nombresDeAmigos);    
+        nombresDeAmigos.push(nombre);
+        limpiarCaja()
+        agregarLista(nombresDeAmigos);   
         
-    }
-    
-    
-    //document.getElementById("listaAmigos").innerHTML = "";
-    console.log(nombresDeAmigos);
-    
-       
+    }  
+ 
 }
 
-//funcion para agregar los amigos al <ul>
-function agregarLista(elementos){
-    let li=0;    
+//Función para agregar el nuevo nombre a la lista
+function agregarLista(lista){   
     const ul=document.getElementById("listaAmigos"); //capturamos
-    li=document.createElement("li"); //creamos el elemento 
-    li.textContent=elementos[elementos.length -1];
-    ul.appendChild(li); //lo agregamos a <ul>
-    console.log(li);       
+    const nuevoElemento =document.createElement("li"); //creamos el elemento 
+    nuevoElemento.textContent=lista[lista.length -1];
+    ul.appendChild(nuevoElemento); //lo agregamos a <ul>       
 
 }
 
 //funcion para limpiar caja
-function cleanCaja(){
-    document.querySelector("#amigo").value="";
+function limpiarCaja(){
+    document.getElementById("amigo").value = "";
 }
 
 
 //funcion para sortear cuando demos click en el boton sortear amigo
 function sortearAmigo(){
-    document.getElementById("listaAmigos").innerHTML = "";
-    let amigoSecreto=0;
+    const ul = document.getElementById("listaAmigos") //capturamos
+    ul.innerHTML = "";
     //const indices=[];
     if(nombresDeAmigos.length == 0 ){
         alert("No es posible sortear, usted debe ingresar los nombres de sus amigos");
 
     } else{
-       // listaRevision.forEach(function(elemento, indice){
-        //indices.push(indice)}); //pasamos 
-        amigoSecreto=Math.floor(Math.random()*nombresDeAmigos.length-1)+1
-        const ul=document.getElementById("listaAmigos"); //capturamos
-        li=document.createElement("li"); //creamos el elemento 
-        li.textContent=`El amigo secreto sorteado es ${nombresDeAmigos[amigoSecreto]}`;
-        ul.appendChild(li); //lo agregamos a <ul>
-        console.log(li);
+        const indice=Math.floor(Math.random()*nombresDeAmigos.length);
+        const resultado=document.createElement("li"); //creamos el elemento 
+        resultado.textContent=`El amigo secreto sorteado es ${nombresDeAmigos[indice]}`;
+        ul.appendChild(resultado); //lo agregamos a <ul>        
     }   
 }
